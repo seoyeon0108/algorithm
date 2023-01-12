f8313699a2e82891a91e4c78702f8cf194b17fb7
seoyeon <97490053+seoyeon0108@users.noreply.github.com>
2023-01-13 00:00:33 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,4 +1,10 @@
 function solution(x) {
-    let plus = [...(x+'')].reduce((acc, cur)=> acc + Number(cur),0 )
-    return x % plus === 0;
+    let num = x;
+    let sum = 0;
+    do {
+        sum += x%10;
+        x = Math.floor(x/10);
+    } while (x>0);
+
+    return !(num%sum);
 }
