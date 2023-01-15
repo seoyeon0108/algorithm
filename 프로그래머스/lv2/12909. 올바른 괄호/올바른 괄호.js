67664e0260202deea0bacdcb029b9882e3afde51
seoyeon <97490053+seoyeon0108@users.noreply.github.com>
2023-01-15 22:28:44 +0900

    [level 2] Title: 올바른 괄호, Time: 6.37 ms, Memory: 38.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,14 @@
+function solution(s){
+    const stack = [];
+    for(let v of s){
+        if(v === '('){
+            stack.push(v)
+        }else{
+            if(stack.length === 0){
+                return false;
+            }
+            stack.pop()
+        }
+    }
+    return stack.length === 0;
+}
