d6020704926551e8ac12e1017c6a2d95f142cd74
seoyeon <97490053+seoyeon0108@users.noreply.github.com>
2023-02-08 00:44:50 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,8 +1,10 @@
 function solution(brown, yellow) {
+    // yellow 약수들 중에서, 가로 세로에 각각 2를 더한값이 brown + yellow와 일치해야 한다.
     const answer = [0, 0];
     
     for(let i = 1; i <= Math.sqrt(yellow); i++){
         let divisor = [0,0];
+        
         if(yellow % i === 0){
             divisor[0] = yellow/i + 2;
             divisor[1] = i + 2;
@@ -13,6 +15,5 @@ function solution(brown, yellow) {
             answer[1] = divisor[1]
         }
     }
-        
     return answer;
 }
