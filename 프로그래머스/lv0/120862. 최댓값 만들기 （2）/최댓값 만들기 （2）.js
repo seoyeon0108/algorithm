5fc17ec66d78bc82771b9ff97d6ebeef6b41e30c
seoyeon <97490053+seoyeon0108@users.noreply.github.com>
2023-02-14 00:38:02 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,20 +1,4 @@
 function solution(numbers) {
-    // 양수, 음수를 분리하여 양수는 내림차순, 음수는 오름차순으로 정렬해 준다.(음수는 작은 수끼리 곱해줘야 최대값이 나오니까)
-    // 양수, 음수 각각의 최댓값을 비교하여 더 큰 값을 반환한다
-    const positive = numbers.filter(v => v >= 0).sort((a,b) => b - a);
-    const negative = numbers.filter(v => v < 0).sort((a,b) => a - b);
-    let posiMax = 0;
-    let negaMax = 0;      
-    
-    if(positive.length <= 1 && negative.length <= 1){
-        return positive[0] * negative[0];
-    }
-    
-    if(positive.length > 1){
-        posiMax = positive[0] * positive[1];
-    }
-    if(negative.length > 1){
-        negaMax = negative[0] * negative[1];
-    }
-    return posiMax > negaMax ? posiMax : negaMax;
+    numbers.sort((a, b) => a - b);
+    return Math.max(numbers[0] * numbers[1], numbers[numbers.length-1] * numbers[numbers.length-2]);
 }
