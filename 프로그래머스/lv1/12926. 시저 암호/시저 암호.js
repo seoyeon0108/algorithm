298864bc5eb27526f4bfe1ac32034a2561addcfc
seoyeon <97490053+seoyeon0108@users.noreply.github.com>
2023-02-20 20:41:14 +0900

    [level 1] Title: 시저 암호, Time: 35.83 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,22 +1,60 @@
-function solution(s, n) {
-    let answer = s.split('');
-    let upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let lower = 'abcdefghijklmnopqrstuvwxyz'
+// function solution(s, n) {
+//     let answer = s.split('');
+//     let upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//     let lower = 'abcdefghijklmnopqrstuvwxyz'
     
-    answer = answer.map(v => {
-        // 대문자 판별
-        let a = v.toUpperCase();
-        let idx = 0;
+//     answer = answer.map(v => {
+//         // 대문자 판별
+//         let a = v.toUpperCase();
+//         let idx = 0;
         
-        if(v === ' ') return ' ';
+//         if(v === ' ') return ' ';
         
-        if(v === a){
-            idx = upper.indexOf(v);
-            return upper[(idx + n) % 26];
-        }else{
-            idx = lower.indexOf(v);
-            return lower[(idx + n) % 26];
+//         if(v === a){
+//             idx = upper.indexOf(v);
+//             return upper[(idx + n) % 26];
+//         }else{
+//             idx = lower.indexOf(v);
+//             return lower[(idx + n) % 26];
+//         }
+//     }) 
+//     return answer.join('');
+// }
+
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+function solution(s, n) {
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lower = "abcdefghijklmnopqrstuvwxyz";
+    var answer= '';
+
+    for(var i =0; i <s.length; i++){
+        var text = s[i];
+        if(text == ' ') {
+            answer += ' '; 
+            continue;
         }
-    }) 
-    return answer.join('');
+        var textArr = upper.includes(text) ? upper : lower;
+        var index = textArr.indexOf(text)+n;
+        if(index >= textArr.length) index -= textArr.length;
+        answer += textArr[index];
+    }
+    return answer;
 }
