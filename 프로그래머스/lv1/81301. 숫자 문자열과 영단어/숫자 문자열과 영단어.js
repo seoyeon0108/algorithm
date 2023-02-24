f39f0467b63cc6b4bbba28079e8a8f5043276df5
seoyeon <97490053+seoyeon0108@users.noreply.github.com>
2023-02-24 19:02:05 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,19 +1,31 @@
+// function solution(s) {
+//     const stringNum = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+//     let plusWord = '';
+//     let result = '';
+    
+//     for(let el of s){
+//         if(Number.isNaN(+el)){
+//             plusWord += el;
+//             if(stringNum.includes(plusWord)){
+//                 result += stringNum.indexOf(plusWord);
+//                 plusWord = '';
+//             }
+//         }else{
+//             result += el;
+//         }
+//     }
+    
+//     return +result;
+// }
+
 function solution(s) {
-    const stringNum = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    let plusWord = '';
-    let result = '';
-    
-    for(let el of s){
-        if(Number.isNaN(+el)){
-            plusWord += el;
-            if(stringNum.includes(plusWord)){
-                result += stringNum.indexOf(plusWord);
-                plusWord = '';
-            }
-        }else{
-            result += el;
-        }
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var answer = s;
+
+    for(let i=0; i< numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        answer = arr.join(i);
     }
-    
-    return +result;
+
+    return Number(answer);
 }
