dfd0169fc207ff91b1090ee1fe548daf12179527
seoyeon <97490053+seoyeon0108@users.noreply.github.com>
2023-03-13 22:29:44 +0900

    [level 1] Title: 소수 찾기, Time: 30.29 ms, Memory: 45.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,12 @@
+function solution(n){
+    let arr = Array(n + 1).fill(true).fill(false, 0, 2);
+    for(let i = 2 ; i * i <= n; i++){
+        if(arr[i]){
+            for(let j = i * i; j <= n; j+=i){
+                arr[j] = false;
+            }
+        }
+    }
+
+    return arr.filter(e => e).length;;
+}
