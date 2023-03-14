fc65356fa5f8e82bf856dd9d8c92ba75a5a7bb3d
seoyeon <97490053+seoyeon0108@users.noreply.github.com>
2023-03-14 23:53:50 +0900

    [level 1] Title: 모의고사, Time: 1.85 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,19 +1,14 @@
 function solution(answers) {
-    let person1 = [];
-    let person2 = [];
-    let person3 = [];
-    // 수포자들의 최대 문재수의 경우만큼 답변작성
-    for(let i = 1; i <= Math.ceil(answers.length/5); i++){
-        person1.push(1, 2, 3, 4, 5);
-        person2.push(2, 1, 2, 3, 2, 4, 2, 5);
-        person3.push(3, 3, 1, 1, 2, 2, 4, 4, 5, 5);
-    }
+    let n = answers.length;
+    let person1 = "12345".repeat(Math.ceil(n/5)).split("");
+    let person2 = "21232425".repeat(Math.ceil(answers.length/8)).split("");
+    let person3 = "3311224455".repeat(Math.ceil(answers.length/10)).split("");
     
     //수포자들이 맞춘 정답 수
     let correct = [0,0,0,0]
-    correct[1] = person1.filter((el,i) => el === answers[i]).length;
-    correct[2] = person2.filter((el,i) => el === answers[i]).length;
-    correct[3] = person3.filter((el,i) => el === answers[i]).length;
+    correct[1] = person1.filter((el,i) => +el === answers[i]).length;
+    correct[2] = person2.filter((el,i) => +el === answers[i]).length;
+    correct[3] = person3.filter((el,i) => +el === answers[i]).length;
     
     // 제일 많이 맞춘 정답수를 구해서 수포자들의 맞춘 정답수랑 비교해서 같은수만 result에 push한다.
     let max = Math.max(...correct);
