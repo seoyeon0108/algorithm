80218d2b235e1adf4114f6c459b24c540ff267ba
seoyeon <97490053+seoyeon0108@users.noreply.github.com>
2023-03-23 00:22:42 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,12 +1,9 @@
 function solution(k, score) {
-    let hallOfFame = [];
-    let rsultScore = [];
-    
-    for(let el of score){
-        hallOfFame.push(el);
-        hallOfFame.sort((a,b) => b - a);
-        hallOfFame = hallOfFame.slice(0,k);
-        rsultScore.push(Math.min(...hallOfFame));
-    }
-    return rsultScore;
+    var answer = [];
+
+    return score.reduce((acc, cur) => {
+        answer.push(cur);
+        answer = answer.sort((a, b) => b - a).slice(0, k);
+        return [...acc, Math.min(...answer)];
+    }, []);
 }
