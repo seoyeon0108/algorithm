ab56ba65ac7068724cd4bc9a35135e40b65e02b6
seoyeon <97490053+seoyeon0108@users.noreply.github.com>
2023-03-30 12:31:55 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -2,7 +2,7 @@ function solution(lottos, win_nums) {
     // 당첨되었을지도 모르는 숫자, 당첨된 숫자가 몇개인지 주해주고 최고, 최저예상을 구해준다.
     const matchNum = [6,6,5,4,3,2,1] 
     
-    let unknown = lottos.filter(v => v === 0).length;
+    let unknown = lottos.filter(v => !v).length;
     let winning = lottos.filter(v => win_nums.includes(v)).length;
     
     return [matchNum[winning + unknown], matchNum[winning]];
