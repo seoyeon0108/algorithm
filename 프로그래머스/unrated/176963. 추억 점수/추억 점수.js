d0b3048ca1e108c33e0e945691ffa94109f9ecc1
seoyeon <97490053+seoyeon0108@users.noreply.github.com>
2023-04-08 11:01:40 +0900

    [unrated] Title: 추억 점수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -1,9 +1,14 @@
+// function solution(name, yearning, photo) {
+//     let score = {};
+    
+//     name.forEach((el,idx) => score[el] = yearning[idx]);
+    
+//     return photo.map(el => 
+//                 el.reduce((acc,cur) => acc + (score[cur] ? score[cur] : 0) ,0
+//             ))
+// }
+
+
 function solution(name, yearning, photo) {
-    let score = {};
-    
-    name.forEach((el,idx) => score[el] = yearning[idx]);
-    
-    return photo.map(el => 
-                el.reduce((acc,cur) => acc + (!!score[cur] ? score[cur] : 0) ,0
-            ))
+    return photo.map((v)=> v.reduce((a, c)=> a += yearning[name.indexOf(c)] ?? 0, 0))
 }
